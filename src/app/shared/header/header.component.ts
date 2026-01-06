f73a826ff4b1a955f3712212dfeecdaa7e1f79c9
NoorMuhammad140 <125381817+NoorMuhammad140@users.noreply.github.com>
2026-01-06 22:15:58 +0500

    New changes..

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -2,12 +2,13 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatMenuModule } from '@angular/material/menu';
 import { RouterModule } from '@angular/router';
 @Component({
   selector: 'app-header',
- imports: [CommonModule, RouterModule, MatToolbarModule, MatButtonModule],  standalone: true,
+ imports: [CommonModule, RouterModule, MatToolbarModule, MatButtonModule, MatMenuModule],  standalone: true,
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
 
